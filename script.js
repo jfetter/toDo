d5edddf4ff118e5a83d5ce5ab433b7f12236d6ce
Jillian Fetter <jillian.fetter@gmail.com>
2015-10-31 16:47:14 -0700

    displays empty shell for html

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ function getClicked(){
 }
 
 
+var newItem = ""
+
 //date.Parse(year-month-day) 
 //returns a date in unix epoch time
 
+//$(".lastTr").append("newItem").removeClass(".lastTd")
+
+//append to an array then push the entire array to the dom
